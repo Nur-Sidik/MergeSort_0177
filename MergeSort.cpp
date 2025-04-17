8f4928858726d0fed583cacb0b8faa98f27cbc4f
Nur-Sidik <nursidik1500@gmail.com>
2025-04-17 11:14:26 +0700

    membuat fungsi rekursi memanggil diri sendiri

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -30,9 +30,12 @@ void input()
     }
 }
 
-void mergesort(int low, int high)
+void mergeSort(int low, int high)
 {
     return;
 }
 
 int mid = (low + high) / 2;
+
+mergeSort(low, mid);
+mergeSort(mid + 1, high);
