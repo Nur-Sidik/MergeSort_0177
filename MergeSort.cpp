5863c3abf255a30d4fe153446ef006a77affc43c
Nur-Sidik <nursidik1500@gmail.com>
2025-04-17 11:02:02 +0700

    masukan index array ke berapa

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -22,4 +22,10 @@ void input()
     cout << "\n--------------------------" << endl;
     cout << "\nInputkan Isi Element Array" << endl;
     cout << "\n--------------------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Array Index ke- " << i << " : ";
+        cin >> arr[i];
+    }
 }
